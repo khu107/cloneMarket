2e1e8157d7d5de43427f144f51e4168505acdcf2
metablog <khu10735@naver.com>
2022-12-31 02:13:56 +0900

    Card

--- src/component/BestCard/index.jsx
+++ src/component/BestCard/index.jsx
@@ -3,7 +3,11 @@ import { CartContainer, Img, H4, Span } from './style';
 
 function index(props) {
   return (
-    <CartContainer>
+    <CartContainer
+      onClick={() => {
+        console.log('hi', props.id);
+      }}
+    >
       <Img src={props.url} alt="alt" srcset="" />
       <H4>{props.title}</H4>
       <Span>{props.price}</Span>
